1e71d7421d9630209f141b0fffc332380cd551ad
asquire <32476451+asquire@users.noreply.github.com>
2018-11-27 19:03:15 -0500

    Set up web service endpoints: /toDoDate and /toDoDateSpan

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const PORT = process.env.PORT || 9888
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/todo', todo)
+  .get('/toDoDate', toDoDate)
+  .get('/toDoDateSpan', toDoDateSpan)
   /*.get('/login', login)*/
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/login', (req, res) => res.render('pages/login'))
+  .get('/login', function (req, res) {
+      //res.send(req.body.topContent)
+      res.render('pages/login')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const pool = new Pool({
@@ -18,18 +22,61 @@ const pool = new Pool({
     ssl: true
 });
 
-function todo() {
+function toDoDate (request, response) {
+    if (typeof(request.query.date_to_start) !== "undefined") {
+        console.log("Date to Start: " + request.query.date_to_start)
+        pool.query('SELECT id, thing_to_do, notes, date_to_start, date_to_be_done FROM to_do_item WHERE date_to_start= date \'' + request.query.date_to_start + '\'', (err, res) => {
+            console.log(JSON.stringify(res.rows))
+            return response.json(res.rows)
+        })
+    } else {
+        console.log('No date given')
+    }
+    if (typeof(request.query.date_to_be_done) !== "undefined") {
+        console.log("Date to Be Done: " + request.query.date_to_be_done)
+        pool.query('SELECT id, thing_to_do, notes, date_to_start, date_to_be_done FROM to_do_item WHERE date_to_be_done = date \'' + request.query.date_to_be_done + '\'', (err, res) => {
+            console.log(JSON.stringify(res.rows))
+            return response.json(res.rows)
+        })
+    } else {
+        console.log('No date given')
+    }
+}
+
+function toDoDateSpan (request, response) {
+    if ((typeof(request.query.date_to_start) !== "undefined") && (typeof(request.query.date_to_be_done) !== "undefined")) {
+        console.log("Date to Start: " + request.query.date_to_start)
+        console.log("Date to Be Done: " + request.query.date_to_be_done)
+        pool.query('SELECT id, thing_to_do, notes, date_to_start, date_to_be_done FROM to_do_item WHERE (date_to_start >= date \'' + request.query.date_to_start + '\' AND date_to_start <= date \'' + request.query.date_to_be_done + '\') OR (date_to_be_done >= date \'' + request.query.date_to_start + '\' AND date_to_be_done <= date \'' + request.query.date_to_be_done + '\')', (err, res) => {
+            console.log(JSON.stringify(res.rows))
+            return response.json(res.rows)
+        })
+    } else {
+        console.log('No date given')
+    }
 }
 
 function login() {
 
 }
 
-function loginPage() {
+function loginForm() {
 
 }
 
 /*
 https://www.freecodecamp.org/forum/t/nodejs-trying-to-get-button-to-navigate-to-other-page-in-folder-structure/160941
 Freecodecamp - Nodejs trying to get button to navigate to other page in folder structure
+
+https://stackoverflow.com/questions/44251142/ejs-dynamic-include
+Stack Overflow - EJS dynamic include
+
+https://www.plus2net.com/html_tutorial/button-linking.php
+plus2net - Linking pages using buttons click event
+
+https://www.postgresql.org/docs/10/functions-datetime.html
+Documentation → PostgreSQL 10 - 9.9. Date/Time Functions and Operators - Chapter 9. Functions and Operators
+
+http://www.informit.com/articles/article.aspx?p=24123&seqNum=4
+informit - Passing Data with Form and URL Variables
  */
--- to-do-sql.sql
+++ to-do-sql.sql
@@ -1,5 +1,7 @@
 -- SQL for To-Do List Project
 
+-- Initial version: 2018/11/24
+
 DROP TABLE IF EXISTS to_do_item, to_do_list_user;
 
 CREATE TABLE to_do_list_user (
@@ -22,3 +24,49 @@ CREATE TABLE to_do_item (
   date_created TIMESTAMP WITH TIME ZONE DEFAULT now(),
   date_modified TIMESTAMP WITH TIME ZONE
 );
+
+
+-- 2018/11/26
+
+DROP TABLE IF EXISTS to_do_item, to_do_list_user;
+
+CREATE TABLE to_do_list_user (
+  id SERIAL PRIMARY KEY,
+  username text NOT NULL,
+  full_name text NOT NULL,
+  password text NOT NULL,
+  date_created TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  date_modified TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+);
+
+CREATE TABLE to_do_item (
+  id SERIAL PRIMARY KEY,
+  user_id_fk INT REFERENCES to_do_list_user(id) NOT NULL,
+  thing_to_do text NOT NULL,
+  notes text,
+  date_to_start TIMESTAMP WITH TIME ZONE NOT NULL,
+  date_to_be_done TIMESTAMP WITH TIME ZONE NOT NULL,
+  date_completed TIMESTAMP WITH TIME ZONE,
+  date_created TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  date_modified TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+);
+
+INSERT INTO to_do_list_user (username, full_name, password)
+VALUES ('asquire', 'Timothy Bohman', 'rights');
+
+INSERT INTO to_do_list_user (username, full_name, password)
+VALUES ('tester', 'Test User', 'access');
+
+INSERT INTO to_do_item (user_id_fk, thing_to_do, notes, date_to_start, date_to_be_done)
+VALUES (1, 'Do laundry', 'Separate batches for whites, colors, and darks', '2018-11-30', '2018-12-01');
+
+INSERT INTO to_do_item (user_id_fk, thing_to_do, notes, date_to_start, date_to_be_done)
+VALUES (1, 'Complete Week 11 Prove assignment', 'Build client-side interaction', '2018-11-29', '2018-12-01');
+
+INSERT INTO to_do_item (user_id_fk, thing_to_do, notes, date_to_start, date_to_be_done)
+VALUES (1, 'Complete Week 12 Prove assignment', 'Finish client-side interaction', '2018-12-06', '2018-12-08');
+
+INSERT INTO to_do_item (user_id_fk, thing_to_do, notes, date_to_start, date_to_be_done)
+VALUES (1, 'Complete the work week', 'The last week of November', '2018-11-26', '2018-11-30');
+
+SELECT * FROM to_do_item;
